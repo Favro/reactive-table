b7bb2555af49ff2d264aed7e950800b2cbca8e31
Erik Olofsson <erik.olofsson@favro.com>
2021-07-16 12:52:46 +0200

    Update Meteor

--- package.js
+++ package.js
@@ -5,8 +5,8 @@ Package.describe({
   git: "https://github.com/aslagle/reactive-table.git"
 });
 
-Package.on_use(function (api) {
-    api.versionsFrom('METEOR@1.10.2');
+Package.onUse(function (api) {
+    api.versionsFrom('METEOR@3.4');
 
     api.use('templating', 'client');
     api.use('jquery', 'client');
@@ -19,20 +19,20 @@ Package.on_use(function (api) {
 
     api.use("fortawesome:fontawesome@4.2.0", 'client', {weak: true});
 
-    api.add_files('lib/reactive_table.html', 'client');
-    api.add_files('lib/filter.html', 'client');
-    api.add_files('lib/reactive_table_i18n.js', 'client');
-    api.add_files('lib/reactive_table.js', 'client');
-    api.add_files('lib/reactive_table.css', 'client');
-    api.add_files('lib/sort.js', 'client');
-    api.add_files('lib/filter.js', ['client', 'server']);
-    api.add_files('lib/server.js', 'server');
+    api.addFiles('lib/reactive_table.html', 'client');
+    api.addFiles('lib/filter.html', 'client');
+    api.addFiles('lib/reactive_table_i18n.js', 'client');
+    api.addFiles('lib/reactive_table.js', 'client');
+    api.addFiles('lib/reactive_table.css', 'client');
+    api.addFiles('lib/sort.js', 'client');
+    api.addFiles('lib/filter.js', ['client', 'server']);
+    api.addFiles('lib/server.js', 'server');
 
     api.export("ReactiveTable", ["client", "server"]);
 });
 
-Package.on_test(function (api) {
-    api.versionsFrom('METEOR@1.10.2');
+Package.onTest(function (api) {
+    api.versionsFrom('METEOR@3.4');
 
     api.use('templating', 'client');
     api.use('jquery', 'client');
@@ -44,43 +44,43 @@ Package.on_test(function (api) {
     api.use("check", "server");
     api.use("audit-argument-checks", "server");
 
-    api.add_files('lib/reactive_table.html', 'client');
-    api.add_files('lib/filter.html', 'client');
-    api.add_files('lib/reactive_table_i18n.js', 'client');
-    api.add_files('lib/reactive_table.js', 'client');
-    api.add_files('lib/reactive_table.css', 'client');
-    api.add_files('lib/sort.js', 'client');
-    api.add_files('lib/filter.js', ['client', 'server']);
-    api.add_files('lib/server.js', 'server');
+    api.addFiles('lib/reactive_table.html', 'client');
+    api.addFiles('lib/filter.html', 'client');
+    api.addFiles('lib/reactive_table_i18n.js', 'client');
+    api.addFiles('lib/reactive_table.js', 'client');
+    api.addFiles('lib/reactive_table.css', 'client');
+    api.addFiles('lib/sort.js', 'client');
+    api.addFiles('lib/filter.js', ['client', 'server']);
+    api.addFiles('lib/server.js', 'server');
 
     api.export("ReactiveTable", ["client", "server"]);
 
     api.use(['tinytest', 'test-helpers'], 'client');
-    api.add_files('test/helpers.js', ['client', 'server']);
-    api.add_files('test/test_collection_argument.js', 'client');
-    api.add_files('test/test_no_data_template.html', 'client');
-    api.add_files('test/test_settings.js', 'client');
-    api.add_files('test/test_fields_tmpl.html', 'client');
-    api.add_files('test/test_fields.js', 'client');
+    api.addFiles('test/helpers.js', ['client', 'server']);
+    api.addFiles('test/test_collection_argument.js', 'client');
+    api.addFiles('test/test_no_data_template.html', 'client');
+    api.addFiles('test/test_settings.js', 'client');
+    api.addFiles('test/test_fields_tmpl.html', 'client');
+    api.addFiles('test/test_fields.js', 'client');
 
     api.use('accounts-password@1.0.6', ['client', 'server']);
-    api.add_files('test/test_reactivity_server.js', 'server');
-    api.add_files('test/test_reactivity.html', 'client');
-    api.add_files('test/test_reactivity.js', 'client');
+    api.addFiles('test/test_reactivity_server.js', 'server');
+    api.addFiles('test/test_reactivity.html', 'client');
+    api.addFiles('test/test_reactivity.js', 'client');
 
-    api.add_files('test/test_sorting.js', 'client');
-    api.add_files('test/test_filtering_server.js', 'server');
-    api.add_files('test/test_filtering.js', 'client');
-    api.add_files('test/test_pagination.js', 'client');
-    api.add_files('test/test_i18n.js', 'client');
-    api.add_files('test/test_events_tmpl.html', 'client');
-    api.add_files('test/test_events.js', 'client');
-    api.add_files('test/test_column_toggles.js', 'client');
-    api.add_files('test/test_multiple_tables.js', 'client');
-    api.add_files('test/test_template.html', 'client');
-    api.add_files('test/test_template.js', 'client');
-    api.add_files('test/test_custom_filters.js', 'client');
+    api.addFiles('test/test_sorting.js', 'client');
+    api.addFiles('test/test_filtering_server.js', 'server');
+    api.addFiles('test/test_filtering.js', 'client');
+    api.addFiles('test/test_pagination.js', 'client');
+    api.addFiles('test/test_i18n.js', 'client');
+    api.addFiles('test/test_events_tmpl.html', 'client');
+    api.addFiles('test/test_events.js', 'client');
+    api.addFiles('test/test_column_toggles.js', 'client');
+    api.addFiles('test/test_multiple_tables.js', 'client');
+    api.addFiles('test/test_template.html', 'client');
+    api.addFiles('test/test_template.js', 'client');
+    api.addFiles('test/test_custom_filters.js', 'client');
 
     api.use("dburles:collection-helpers@1.0.1", "client");
-    api.add_files("test/test_compatibility.js", "client");
+    api.addFiles("test/test_compatibility.js", "client");
 });
